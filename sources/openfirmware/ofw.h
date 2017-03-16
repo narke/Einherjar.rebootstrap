ae5508b51b1ea976dc8a0f7aaf5f279d2d0a5ae0
Konstantin Tcholokachvili <tcholoka@gmail.com>
2017-03-16 22:59:33 +0100

    Made some changes to start implementing physical memory allocator

--- sources/openfirmware/ofw.h
+++ sources/openfirmware/ofw.h
@@ -10,12 +10,14 @@
 #include <lib/typedefs.h>
 #include <lib/stdarg.h>
 
-#define MEMMAP_MAX_RECORDS  32
-#define MAX_OFW_ARGS        12
+#define TASKMAP_MAX_RECORDS 			32
+#define MEMMAP_MAX_RECORDS  			32
+#define BOOTINFO_TASK_NAME_BUFFER_LENGTH	32
+#define MAX_OFW_ARGS				12
 
-#define OFW_TREE_PATH_MAX_LEN           256
-#define OFW_TREE_PROPERTY_MAX_NAMELEN   32
-#define OFW_TREE_PROPERTY_MAX_VALUELEN  64
+#define OFW_TREE_PATH_MAX_LENGTH		256
+#define OFW_TREE_PROPERTY_MAX_NAME_LENGTH	32
+#define OFW_TREE_PROPERTY_MAX_VALUE_LENGTH	64
 
 typedef sysarg_t ofw_arg_t;
 typedef native_t ofw_ret_t;
@@ -26,13 +28,73 @@ typedef uint32_t phandle;
 /*
  * OpenFirmware command structure
  */
-typedef struct {
+typedef struct
+{
 	ofw_arg_t service;             /* Command name. */
 	ofw_arg_t nargs;               /* Number of in arguments. */
 	ofw_arg_t nret;                /* Number of out arguments. */
 	ofw_arg_t args[MAX_OFW_ARGS];  /* List of arguments. */
 } ofw_args_t;
 
+typedef struct
+{
+	void *addr;
+	size_t size;
+	char name[BOOTINFO_TASK_NAME_BUFFER_LENGTH];
+} utask_t;
+
+typedef struct
+{
+	size_t count;
+	utask_t tasks[TASKMAP_MAX_RECORDS];
+} taskmap_t;
+
+typedef struct
+{
+	void *start;
+	size_t size;
+} memzone_t;
+
+typedef struct
+{
+	uint64_t total;
+	size_t count;
+	memzone_t zones[MEMMAP_MAX_RECORDS];
+} memmap_t;
+
+typedef struct
+{
+	uintptr_t base;
+	size_t size;
+} ballocs_t;
+
+typedef struct
+{
+	char name[OFW_TREE_PROPERTY_MAX_NAME_LENGTH];
+	size_t size;
+	void *value;
+} ofw_tree_property_t;
+
+typedef struct
+{
+	struct ofw_tree_node_t *parent;
+	struct ofw_tree_node_t *peer;
+	struct ofw_tree_node_t *child;
+
+	phandle node_handle;
+	char da_name;
+	size_t properties;
+	ofw_tree_property_t *property;
+	void *device;
+} ofw_tree_node_t;
+
+typedef struct
+{
+	memmap_t memmap;
+	taskmap_t taskmap;
+	ballocs_t ballocs;
+	ofw_tree_node_t *ofw_root;
+} bootinfo_t;
 
 extern phandle ofw_chosen;
 extern ihandle ofw_stdout;
